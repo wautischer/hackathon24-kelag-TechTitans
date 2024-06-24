1acf85c88d9489a2582b704faee4ed4ebfdb3868
gianlucadB <C_HACK4@kelagat.onmicrosoft.com>
2024-06-24 14:10:28 +0200

    see all issues

--- AI_Ticketmanager/AI_Ticketmanager/Program.cs
+++ AI_Ticketmanager/AI_Ticketmanager/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddSingleton<TicketService>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- AI_Ticketmanager/AI_Ticketmanager/obj/Debug/net8.0/AI_Ticketmanager.AssemblyInfo.cs
+++ AI_Ticketmanager/AI_Ticketmanager/obj/Debug/net8.0/AI_Ticketmanager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e79baf5b2138081038189bb89f88b289753092ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3d9d85693d648bc38fbb3a53708e5bd3243f33f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
