ba8e4356f2410e75122236db5e373baed7b5a1c5
gianlucadB <C_HACK4@kelagat.onmicrosoft.com>
2024-06-25 08:11:41 +0200

    sql to azure

--- AI_Ticketmanager/AI_Ticketmanager/obj/Debug/net8.0/AI_Ticketmanager.AssemblyInfo.cs
+++ AI_Ticketmanager/AI_Ticketmanager/obj/Debug/net8.0/AI_Ticketmanager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a131ba2457f6e37abfb0c0ea8126ebb65013c53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ee41e305b78a72b4a56925ff79e8935671810cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
