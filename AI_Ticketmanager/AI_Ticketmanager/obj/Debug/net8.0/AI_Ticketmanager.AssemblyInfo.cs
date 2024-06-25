b937701f21191ed29fdabc22fada2c1cd36e3cac
wautischer <wautischer@outlook.com>
2024-06-25 10:51:26 +0200

    useless files

--- AI_Ticketmanager/AI_Ticketmanager/obj/Debug/net8.0/AI_Ticketmanager.AssemblyInfo.cs
+++ AI_Ticketmanager/AI_Ticketmanager/obj/Debug/net8.0/AI_Ticketmanager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d98f537073927df609275c3b961172aa2cbf013")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09843c518a251771faf75ea53e7a064125cf6e23")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI_Ticketmanager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
